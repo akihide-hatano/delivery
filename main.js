0ae7972f702bfa520d94ce8f3d1617367def476a
akihide-hatano <a.h.19841202@gmail.com>
2025-03-07 14:34:22 +0900

    firebaseから商品のデータを取得して表示する

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,44 +18,70 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app); // app は initializeApp() で初期化したもの
-    // ラーメン画像 URL の取得と表示
-    const imageRefRamen = ref(storage, 'ramen1.jpg');
-  
-    getDownloadURL(imageRefRamen)
-      .then((url) => {
-        const imgElement = document.getElementById('ramen-image');
-        imgElement.src = url;
-        
-      })
-      .catch((error) => {
-        console.error("Error getting download URL:", error);
-      });
-  
-    // 玉子ラーメン画像 URL の取得と表示
-    const imageRefEgg = ref(storage, 'ramen2.png');
-  
-    getDownloadURL(imageRefEgg)
-      .then((url) => {
-        const imgElement = document.getElementById('egg-men-img');
-        imgElement.src = url;
-        
-      })
-      .catch((error) => {
-        console.error("Error getting download URL:", error);
-      });
+const db = getFirestore(app); // app は initializeApp() で初期化したもの
 
-    // チャーシューラーメン画像 URL の取得と表示
-    const imageRefTyashu = ref(storage, 'ramen3.png');
+    // // ラーメン画像 URL の取得と表示
+    // const imageRefRamen = ref(storage, 'ramen1.jpg');
   
-    getDownloadURL(imageRefTyashu)
-      .then((url) => {
-        const imgElement = document.getElementById('tyashu-men-img');
-        imgElement.src = url;
+    // getDownloadURL(imageRefRamen)
+    //   .then((url) => {
+    //     const imgElement = document.getElementById('ramen-image');
+    //     imgElement.src = url;
         
-      })
-      .catch((error) => {
-        console.error("Error getting download URL:", error);
-      });
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error getting download URL:", error);
+    //   });
+  
+    // // 玉子ラーメン画像 URL の取得と表示
+    // const imageRefEgg = ref(storage, 'ramen2.png');
+  
+    // getDownloadURL(imageRefEgg)
+    //   .then((url) => {
+    //     const imgElement = document.getElementById('egg-men-img');
+    //     imgElement.src = url;
+        
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error getting download URL:", error);
+    //   });
+
+    // // チャーシューラーメン画像 URL の取得と表示
+    // const imageRefTyashu = ref(storage, 'ramen3.png');
+  
+    // getDownloadURL(imageRefTyashu)
+    //   .then((url) => {
+    //     const imgElement = document.getElementById('tyashu-men-img');
+    //     imgElement.src = url;
+        
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error getting download URL:", error);
+    //   });
+
+
+      // 商品情報の取得
+  getDocs(collection(db, 'items'))
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const itemData = doc.data();
+      const itemId = doc.id;
+
+      // 商品情報を表示
+      const itemElement = document.getElementById(itemId);
+      const imgElement = itemElement.querySelector('img');
+      const nameElement = itemElement.querySelector('.itemName');
+      const priceElement = itemElement.querySelector('.itemPrice');
+
+      imgElement.src = itemData.imageUrl;
+      nameElement.textContent = itemData.name;
+      priceElement.textContent = `値段${itemData.price}円`;
+    });
+  })
+  .catch((error) => {
+    console.error("Error getting documents:", error);
+  });
+
 
 
 
